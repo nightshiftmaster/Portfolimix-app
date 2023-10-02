948c69e0d8f21323a498ab9d9f5c9e0f6bfa5230
vlad medvede <vladmedvede@vlads-MacBook-Air.local>
2023-10-02 19:20:36 +0300

    update homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,28 @@
 import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital products.
+        </h1>
+        <p className={styles.desc}>
+          Turning your Idea into Reality. We bring together the teams from the
+          global tech industry.
+        </p>
+        <button className={styles.button}>See my works</button>
+      </div>
+      <div className={styles.item}>
+        <Image
+          width={1000}
+          height={1000}
+          className={styles.image}
+          src="/home3.png"
+          alt="home picture"
+        />
+      </div>
+    </div>
+  );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,34 @@
 import React from "react";
 import styles from "./footer.module.css";
 import Image from "next/image";
+// import ReactLogo from "../../../public/1.png";
 
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div>@2023 All rights reserved</div>
-      <div>
-        <Image src="" alt="My website" />
+      <div className={styles.social}>
+        <Image
+          width={20}
+          height={20}
+          className={styles.icon}
+          src="/facebook.png"
+          alt="facebook icon"
+        />
+        <Image
+          width={20}
+          height={20}
+          className={styles.icon}
+          src="/youtube.png"
+          alt="youtube icon"
+        />
+        <Image
+          width={20}
+          height={20}
+          className={styles.icon}
+          src="/insta.png"
+          alt="instagram icon"
+        />
       </div>
     </div>
   );
