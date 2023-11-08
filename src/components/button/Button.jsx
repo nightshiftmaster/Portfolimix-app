852e9e52e18b3d4ed66739eab340d8f3273b457b
vlad medvede <vladmedvede@vlads-MacBook-Air.local>
2023-11-08 18:17:37 +0200

    button

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import styles from "./button.module.css";
 import Link from "next/link";
 
-const Button = ({ text, url }) => (
-  <Link href={url}>
-    <div className={styles.container}>{text}</div>
-  </Link>
-);
-
+const Button = ({ text, url }) => {
+  return (
+    <Link href={url}>
+      <div className={styles.container}>{text}</div>
+    </Link>
+  );
+};
 export default Button;
