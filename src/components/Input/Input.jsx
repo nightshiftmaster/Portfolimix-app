5803aa77f5ad9f9716f1e31930de7ed7dadc9912
vlad medvede <vladmedvede@vlads-MacBook-Air.local>
2023-11-08 14:51:40 +0200

    input3

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -3,31 +3,29 @@ import React from "react";
 import { Field, useField } from "formik";
 import { IoMdAlert } from "react-icons/io";
 
-const Input = ({ name, touched = null, errors = null }) => {
-  return (
-    <div className={styles.inputContainer}>
-      <Field
-        name={name}
-        placeholder={name}
-        className={styles.input}
-        style={{
-          outline:
-            touched && errors
-              ? "2px solid red"
-              : touched
-              ? "2px solid white"
-              : null,
-        }}
-        required
-      />
-      <IoMdAlert
-        className={styles.alerticon}
-        display={touched && errors ? "block" : "none"}
-      />
-      {touched && errors ? <div className={styles.error}>{errors}</div> : null}
-    </div>
-  );
-};
+const Input = ({ name, touched = null, errors = null }) => (
+  <div className={styles.inputContainer}>
+    <Field
+      name={name}
+      placeholder={name}
+      className={styles.input}
+      style={{
+        outline:
+          touched && errors
+            ? "2px solid red"
+            : touched
+            ? "2px solid white"
+            : null,
+      }}
+      required
+    />
+    <IoMdAlert
+      className={styles.alerticon}
+      display={touched && errors ? "block" : "none"}
+    />
+    {touched && errors ? <div className={styles.error}>{errors}</div> : null}
+  </div>
+);
 // const TextArea = ({ name, touched, errors }) => {
 //   const [field, meta] = useField(name);
 //   return (
