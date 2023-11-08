4deecf9926be9a2f6884724dac671ba87f865ec7
vlad medvede <vladmedvede@vlads-MacBook-Air.local>
2023-11-08 19:33:25 +0200

    email update

--- src/app/api/email/route.js
+++ src/app/api/email/route.js
@@ -26,11 +26,14 @@ export const POST = async (request) => {
       console.log(error);
       console.log(info);
     });
-    const responce = new NextResponse("Thank you for feedback!", {
+    return new NextResponse("Thank you for feedback!", {
       status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
     });
-    responce.headers.set("Access-Control-Allow-Origin", "*");
-    return responce;
   } catch (e) {
     return new NextResponse(
       "Email not sent. Check your credentials or try again later",
