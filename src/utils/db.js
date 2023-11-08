b0c422117e3dcf1b29b17168ff49b4759e31b9f7
vlad medvede <vladmedvede@vlads-MacBook-Air.local>
2023-11-09 01:39:12 +0200

    mongo2

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 async function connect() {
   try {
-    await mongoose.connect(
-      "mongodb+srv://nightshiftmaster:Vlad19820708@cluster0.lrcjkhf.mongodb.net/test?retryWrites=true&w=majority"
-    );
+    await mongoose.connect(process.env.MONGO);
   } catch (err) {
     throw new Error("Connection failed");
   }
