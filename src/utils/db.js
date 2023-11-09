75a55e638547debf556c5a05c47dcf1d631fe436
vlad medvede <vladmedvede@vlads-MacBook-Air.local>
2023-11-09 14:41:32 +0200

    work route remove

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
-async function connect() {
+const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO);
   } catch (err) {
     throw new Error("Connection failed");
   }
-}
+};
 
 export default connect;
