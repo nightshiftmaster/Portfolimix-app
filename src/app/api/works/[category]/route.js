a382f93a7d5b9f3f774bab8e25dd7c9165c1e076
vlad medvede <vladmedvede@vlads-MacBook-Air.local>
2023-11-09 15:32:42 +0200

    work route

--- src/app/api/works/[category]/route.js
+++ src/app/api/works/[category]/route.js
@@ -1,14 +1,14 @@
 import { NextResponse } from "next/server";
-import connect from "@/utils/db";
-import Work from "@/models/Work";
+import connect from "../../../../utils/db";
+import Work from "../../../../models/Work";
 
 export const GET = async (request, { params }) => {
   const { category } = params;
   await connect();
   try {
     const post = await Work.find({ category });
-    return new Response(JSON.stringify(post), { status: 200 });
+    return new NextResponse(JSON.stringify(post), { status: 200 });
   } catch (error) {
-    return new Response("Database error", { status: 500 });
+    return new NextResponse("Database error", { status: 500 });
   }
 };
