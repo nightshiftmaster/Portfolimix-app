eebe2dee5ad575262411fb578cb1f29ce17a96c1
vlad medvede <vladmedvede@vlads-MacBook-Air.local>
2023-11-09 02:01:30 +0200

    json

--- src/app/api/works/[category]/route.js
+++ src/app/api/works/[category]/route.js
@@ -6,8 +6,8 @@ export const GET = async (request, { params }) => {
   const { category } = params;
   await connect();
   try {
-    // const post = await Work.find({ category });
-    return new NextResponse("success", { status: 200 });
+    const post = await Work.find({ category });
+    return new NextResponse(JSON.stringify(post), { status: 200 });
   } catch (error) {
     return new NextResponse("Database error", { status: 500 });
   }
