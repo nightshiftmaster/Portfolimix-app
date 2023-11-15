210ee99e6c1203c260ea285c00d87fa170b23b25
vlad medvede <vladmedvede@vlads-MacBook-Air.local>
2023-11-16 00:48:55 +0200

    footer socials

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,33 +1,35 @@
 import React from "react";
 import styles from "./footer.module.css";
 import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div className={styles.date}>@2023 All rights reserved</div>
       <div className={styles.social}>
-        <Image
-          width={20}
-          height={20}
-          className={styles.icon}
-          src="/facebook.png"
-          alt="facebook icon"
-        />
-        <Image
-          width={20}
-          height={20}
-          className={styles.icon}
-          src="/youtube.png"
-          alt="youtube icon"
-        />
-        <Image
-          width={20}
-          height={20}
-          className={styles.icon}
-          src="/insta.png"
-          alt="instagram icon"
-        />
+        <Link href="https://github.com/nightshiftmaster">
+          <Image width={21} height={21} src="/github.png" alt="github icon" />
+        </Link>
+
+        <Link href="https://www.linkedin.com/in/vlad-medvedev-069b6425b/">
+          <Image
+            width={22}
+            height={22}
+            src="/linkedin.png"
+            alt="linkedin icon"
+          />
+        </Link>
+
+        <Link href="https://www.facebook.com/profile.php?id=1198533625&locale=he_IL">
+          <Image
+            width={20}
+            height={20}
+            className={styles.icon}
+            src="/facebook.png"
+            alt="facebook icon"
+          />
+        </Link>
       </div>
     </div>
   );
